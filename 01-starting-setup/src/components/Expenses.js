a9cc61df45cddede7bcead710587bcd636262d4c
AndrewSimons3 <simdrew07@gmail.com>
2022-06-12 07:43:41 -0500

    finished initial proj

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+import './Expenses.css';
 
 const Expenses = ({expenses}) => {
   return (
-		<div className="expenses">
+		<Card className="expenses">
 			<ExpenseItem
 				title={expenses[0].title}
 				amount={expenses[0].amount}
@@ -24,7 +26,7 @@ const Expenses = ({expenses}) => {
 				amount={expenses[3].amount}
 				date={expenses[3].date}
 			></ExpenseItem>
-		</div>
+		</Card>
 	);
 }
 
