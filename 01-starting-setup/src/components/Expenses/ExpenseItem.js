60c9f927c19ad13761dbb8dc3e47d47faa7edf8f
AndrewSimons3 <simdrew07@gmail.com>
2022-06-12 08:39:31 -0500

    added useState to manage title

---
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import { useState } from 'react';
+
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+	
+	const [title, setTitle] = useState(props.title);
+
+	const changeTitleHandler = () => {
+		setTitle('Updated!');
+	}
+
+	return (
+		<Card className='expense-item'>
+      <ExpenseDate date={props.date}/>
+			<div className='expense-item__description'>
+				<h2>{title}</h2>
+				<div className='expense-item__price'>{props.amount}</div>
+			</div>
+			<button onClick={changeTitleHandler}>Change Title</button>
+		</Card>
+	);
+};
+
+export default ExpenseItem;
